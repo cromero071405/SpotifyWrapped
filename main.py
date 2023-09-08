b5b7bdc6cd0ed8ee03b8b675813a2397a40c2c4c
cromero071405 <cesardr1@uci.edu>
2023-09-08 10:46:29 -0700

    Finished Inserting Data Into Google Sheet

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import time
+
 import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import pandas as pd
+import gspread
 
 
 def authenticate_spotify():
@@ -32,17 +35,39 @@ def get_track_features(sp, track_id):
     return track_info
 
 
+def insert_to_gsheet(sp, gc, time_period, track_ids):
+    # loop over track ids
+    tracks = []
+    for i in range(len(track_ids)):
+        time.sleep(.5)
+        track = get_track_features(sp, track_ids[i])
+        tracks.append(track)
+    # create dataset
+    df = pd.DataFrame(tracks, columns=["name", "album", "artist", "spotify_url", "album_cover"])
+    # insert into google sheet
+    sh = gc.open("My Spotify Wrapped")
+    worksheet = sh.worksheet(f"{time_period}")
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+    print("Done")
+
+
 def main():
     client_id, client_secret, redirect_uri, scope = authenticate_spotify()
 
     # Initiating the sp object
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret,
                                                    redirect_uri=redirect_uri, scope=scope))
+    # Initializing the gc object
+    gc = gspread.service_account(filename="my-spotify-wrapped-test-398417-35f546937d41.json")
 
     top_tracks_long = sp.current_user_top_tracks()
     track_ids = get_track_ids(top_tracks_long)
 
-    print(get_track_features(sp, track_ids[0]))
+    time_ranges = ["short_term", "medium_term", "long_term"]
+    for time_period in time_ranges:
+        top_tracks = sp.current_user_top_tracks(limit=20, offset=0, time_range=time_period)
+        track_ids = get_track_ids(top_tracks)
+        insert_to_gsheet(sp, gc, time_period, track_ids)
 
 
 if __name__ == "__main__":
