cac806397d0aabe1d34094edc24c69e21245ea7a
cromero071405 <cesardr1@uci.edu>
2023-09-08 09:39:46 -0700

    Finished writing code to view my top tracks
    In the dictionary returned in "sp.current_user_top_tracks()", I needed to only retrieve the track ids. So, once I did, I created another function called "get_track_features()" that would get the information I wanted from those ids gathered. Ultimately, now I can see what is number 1 track, number 3 track, etc.

--- main.py
+++ main.py
@@ -13,12 +13,36 @@ def authenticate_spotify():
     return spotipy_client_id, spotipy_client_secret, spotipy_redirect_uri, scope
 
 
+def get_track_ids(time_frame):
+    track_ids = []
+    for song in time_frame["items"]:
+        track_ids.append(song["id"])
+    return track_ids
+
+
+def get_track_features(sp, track_id):
+    meta = sp.track(track_id)
+    # meta
+    name = meta["name"]
+    album = meta["album"]["name"]
+    artist = meta["album"]["artists"][0]["name"]
+    spotify_url = meta["external_urls"]["spotify"]
+    album_cover = meta["album"]["images"][0]["url"]
+    track_info = [name, album, artist, spotify_url, album_cover]
+    return track_info
+
+
 def main():
     client_id, client_secret, redirect_uri, scope = authenticate_spotify()
 
     # Initiating the sp object
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret,
                                                    redirect_uri=redirect_uri, scope=scope))
+
+    top_tracks_long = sp.current_user_top_tracks()
+    track_ids = get_track_ids(top_tracks_long)
+
+    print(get_track_features(sp, track_ids[0]))
 
 
 if __name__ == "__main__":
